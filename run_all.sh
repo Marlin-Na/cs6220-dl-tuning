bb79009093bb23bffc148c0cdc7bc8f969440509
Jialin Ma <marlin@inventati.org>
2019-09-07 09:14:23 -0400

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -20,17 +20,17 @@ python retrain.py --image_dir=$TRAIN_DATA_DIR \
        --output_labels=$MODEL_DIR/trained_model/retrained_labels.txt \
        --summaries_dir=$MODEL_DIR/summaries > $MODEL_DIR/train.log 2> $MODEL_DIR/train.err
 
-# for file in $TEST_DATA_DIR/*; do
-#     class=$(
-#         python label_image.py \
-#                --graph=$MODEL_DIR/trained_model/retrained_graph.pb \
-#                --labels=$MODEL_DIR/trained_model/retrained_labels.txt \
-#                --input_layer=Placeholder \
-#                --output_layer=final_result \
-#                --image=$file 2> $MODEL_DIR/test.err | head -n 1
-#          )
-#     echo $file $class >> $MODEL_DIR/test_res
-# done
+for file in $TEST_DATA_DIR/*; do
+    class=$(
+        python label_image.py \
+               --graph=$MODEL_DIR/trained_model/retrained_graph.pb \
+               --labels=$MODEL_DIR/trained_model/retrained_labels.txt \
+               --input_layer=Placeholder \
+               --output_layer=final_result \
+               --image=$file 2> $MODEL_DIR/test.err | head -n 1
+         )
+    echo $file $class >> $MODEL_DIR/test_res
+done
 
 
 # ========== Train 200 (resize) ====================
@@ -52,6 +52,17 @@ python retrain.py --image_dir=$TRAIN_DATA_DIR \
        --output_labels=$MODEL_DIR/trained_model/retrained_labels.txt \
        --summaries_dir=$MODEL_DIR/summaries > $MODEL_DIR/train.log 2> $MODEL_DIR/train.err
 
+for file in $TEST_DATA_DIR/*; do
+    class=$(
+        python label_image.py \
+               --graph=$MODEL_DIR/trained_model/retrained_graph.pb \
+               --labels=$MODEL_DIR/trained_model/retrained_labels.txt \
+               --input_layer=Placeholder \
+               --output_layer=final_result \
+               --image=$file 2> $MODEL_DIR/test.err | head -n 1
+         )
+    echo $file $class >> $MODEL_DIR/test_res
+done
 
 # ========== Train 2000 ====================
 
@@ -72,6 +83,18 @@ python retrain.py --image_dir=$TRAIN_DATA_DIR \
        --output_labels=$MODEL_DIR/trained_model/retrained_labels.txt \
        --summaries_dir=$MODEL_DIR/summaries > $MODEL_DIR/train.log 2> $MODEL_DIR/train.err
 
+for file in $TEST_DATA_DIR/*; do
+    class=$(
+        python label_image.py \
+               --graph=$MODEL_DIR/trained_model/retrained_graph.pb \
+               --labels=$MODEL_DIR/trained_model/retrained_labels.txt \
+               --input_layer=Placeholder \
+               --output_layer=final_result \
+               --image=$file 2> $MODEL_DIR/test.err | head -n 1
+         )
+    echo $file $class >> $MODEL_DIR/test_res
+done
+
 # ========== Train 2000 (resize) ====================
 
 TRAIN_DATA_DIR=raw_data/train2000_resize/
@@ -90,5 +113,17 @@ python retrain.py --image_dir=$TRAIN_DATA_DIR \
        --output_graph=$MODEL_DIR/trained_model/retrained_graph.pb \
        --output_labels=$MODEL_DIR/trained_model/retrained_labels.txt \
        --summaries_dir=$MODEL_DIR/summaries > $MODEL_DIR/train.log 2> $MODEL_DIR/train.err
+
+for file in $TEST_DATA_DIR/*; do
+    class=$(
+        python label_image.py \
+               --graph=$MODEL_DIR/trained_model/retrained_graph.pb \
+               --labels=$MODEL_DIR/trained_model/retrained_labels.txt \
+               --input_layer=Placeholder \
+               --output_layer=final_result \
+               --image=$file 2> $MODEL_DIR/test.err | head -n 1
+         )
+    echo $file $class >> $MODEL_DIR/test_res
+done
 
 
